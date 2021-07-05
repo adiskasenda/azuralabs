49190a932866b296f877fa371c180eb632facbd8
adiskasenda <adis.kasenda@gmail.com>
2021-07-05 21:38:34 +0700

    menambah datatable untuk menampilkan data diklat

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -5,4 +5,7 @@
     <title>@yield('title')</title>
     {{-- css bootstrap --}}
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    {{-- css datatable --}}
+    <link rel="stylesheet" href="//cdn.datatables.net/1.10.25/css/jquery.dataTables.min.css">
+
 </head>
--- resources/views/menu/beranda.blade.php
+++ resources/views/menu/beranda.blade.php
@@ -1,6 +1,30 @@
 @extends('layouts/main')
 @section('title', 'Beranda')
 @section('content')
-
-
+<div class="row"></div>
+<div class="row">
+<div class="col-md-1"></div>
+    <div class="col-md-10">
+        <table class="table table-striped" id="myTable" class="display">
+            <thead class="thead-dark">
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Nama</th>
+                    <th scope="col">Jabatan</th>
+                    <th scope="col">Foto</th>
+                    <th scope="col">Aksi</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Row 1 Data 1</td>
+                    <td>Row 1 Data 2</td>
+                    <td>Row 1 Data 2</td>
+                    <td>Row 1 Data 2</td>
+                    <td>Row 1 Data 2</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</div>
 @endsection
