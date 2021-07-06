039e223a973f3803ecfa8f20afde15e9cfce37b1
adiskasenda <adis.kasenda@gmail.com>
2021-07-06 14:24:47 +0700

    membuat controller dan model diklat

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('menu/beranda');
-});
+// Route::get('/', function () {
+//     return view('menu/beranda');
+// });
+Route::get('/', [App\Http\Controllers\DiklatController::class, 'index']);
 
